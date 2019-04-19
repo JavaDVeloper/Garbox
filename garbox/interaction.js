c4b17608635ce40136215321f6995ee8c8b8e9fe
JavaDVeloper <36056458+JavaDVeloper@users.noreply.github.com>
2019-04-19 16:38:27 +0100

    Add files via upload

---
+++ garbox/interaction.js
@@ -0,0 +1,9 @@
+
+document.getElementById("driver-login").addEventListener("keyup", changeColour);
+
+
+
+function changeColour(){
+document.getElementById("driver-login").style.color= "#870d4e";
+document.getElementById("driver-login").value = "";	
+}
